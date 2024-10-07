70f07dbe69dd39a0ca29283c108995b21db4eb8c
Sakura <69903799+kafuneri@users.noreply.github.com>
2024-10-07 11:58:00 +0800

    Update friends.ts

--- server/src/services/friends.ts
+++ server/src/services/friends.ts
@@ -24,8 +24,8 @@ export function FriendService() {
                 return { friend_list, apply_list };
             })
                 .post('/', async ({ admin, uid, username, set, body: { name, desc, avatar, url } }) => {
-                    const config = ClientConfig()
-                    const enable = await config.getOrDefault('friend_apply_enable', true)
+                    const config = ClientConfig();
+                    const enable = await config.getOrDefault('friend_apply_enable', true);
                     if (!enable && !admin) {
                         set.status = 403;
                         return 'Friend Link Apply Disabled';
@@ -66,121 +66,14 @@ export function FriendService() {
                         const webhookUrl = await ServerConfig().get(Config.webhookUrl) || env.WEBHOOK_URL;
                         const content = `${env.FRONTEND_URL}/friends\n${username} 申请友链: ${name}\n${desc}\n${url}`;
                         // notify
-                        await notify(webhookUrl, content);
+                        if (webhookUrl) { // 确保 webhookUrl 存在
+                            await notify(content);
+                        } else {
+                            console.error('Webhook URL is not defined.');
+                        }
                     }
                     return 'OK';
                 }, {
                     body: t.Object({
                         name: t.String(),
-                        desc: t.String(),
-                        avatar: t.String(),
-                        url: t.String(),
-                    })
-                })
-                .put('/:id', async ({ admin, uid, username, set, params: { id }, body: { name, desc, avatar, url, accepted } }) => {
-                    const config = ClientConfig()
-                    const enable = await config.getOrDefault('friend_apply_enable', true)
-                    if (!enable && !admin) {
-                        set.status = 403;
-                        return 'Friend Link Apply Disabled';
-                    }
-                    if (!uid) {
-                        set.status = 401;
-                        return 'Unauthorized';
-                    }
-                    const exist = await db.query.friends.findFirst({
-                        where: eq(friends.id, parseInt(id)),
-                    });
-                    if (!exist) {
-                        set.status = 404;
-                        return 'Not found';
-                    }
-                    if (!admin && exist.uid !== uid) {
-                        set.status = 403;
-                        return 'Permission denied';
-                    }
-                    if (!admin) {
-                        accepted = 0;
-                    }
-                    function wrap(s: string | undefined) {
-                        return s ? s.length === 0 ? undefined : s : undefined;
-                    }
-                    await db.update(friends).set({
-                        name: wrap(name),
-                        desc: wrap(desc),
-                        avatar: wrap(avatar),
-                        url: wrap(url),
-                        accepted: accepted === undefined ? undefined : accepted,
-                    }).where(eq(friends.id, parseInt(id)));
-                    if (!admin) {
-                        const webhookUrl = await ServerConfig().get(Config.webhookUrl) || env.WEBHOOK_URL;
-                        const content = `${env.FRONTEND_URL}/friends\n${username} 更新友链: ${name}\n${desc}\n${url}`;
-                        // notify
-                        await notify(webhookUrl, content);
-                    }
-                    return 'OK';
-                }, {
-                    body: t.Object({
-                        name: t.String(),
-                        desc: t.String(),
-                        avatar: t.Optional(t.String()),
-                        url: t.String(),
-                        accepted: t.Optional(t.Integer()),
-                    })
-                })
-                .delete('/:id', async ({ admin, uid, set, params: { id } }) => {
-                    if (!uid) {
-                        set.status = 401;
-                        return 'Unauthorized';
-                    }
-                    const exist = await db.query.friends.findFirst({
-                        where: eq(friends.id, parseInt(id)),
-                    });
-                    if (!exist) {
-                        set.status = 404;
-                        return 'Not found';
-                    }
-                    if (!admin && exist.uid !== uid) {
-                        set.status = 403;
-                        return 'Permission denied';
-                    }
-                    await db.delete(friends).where(eq(friends.id, parseInt(id)));
-                    return 'OK';
-                })
-        )
-}
-
-export async function friendCrontab(env: Env, ctx: ExecutionContext) {
-    const config = ServerConfig()
-    const enable = await config.getOrDefault('friend_crontab', true)
-    const ua = await config.get('friend_ua') || 'Rin-Check/0.1.0'
-    if (!enable) {
-        console.info('friend crontab disabled')
-        return
-    }
-    const db = drizzle(env.DB, { schema: schema })
-    const friend_list = await db.query.friends.findMany()
-    console.info(`total friends: ${friend_list.length}`)
-    let health = 0
-    let unhealthy = 0
-    for (const friend of friend_list) {
-        console.info(`checking ${friend.name}: ${friend.url}`)
-        try {
-            const response = await fetch(new Request(friend.url, { method: 'GET', headers: { 'User-Agent': ua } }))
-            console.info(`response status: ${response.status}`)
-            console.info(`response statusText: ${response.statusText}`)
-            if (response.ok) {
-                ctx.waitUntil(db.update(schema.friends).set({ health: "" }).where(eq(schema.friends.id, friend.id)))
-                health++
-            } else {
-                ctx.waitUntil(db.update(schema.friends).set({ health: `${response.status}` }).where(eq(schema.friends.id, friend.id)))
-                unhealthy++
-            }
-        } catch (e: any) {
-            console.error(e.message)
-            ctx.waitUntil(db.update(schema.friends).set({ health: e.message }).where(eq(schema.friends.id, friend.id)))
-            unhealthy++
-        }
-    }
-    console.info(`update friends health done. Total: ${health + unhealthy}, Healthy: ${health}, Unhealthy: ${unhealthy}`)
-}
+                        desc: t.S
