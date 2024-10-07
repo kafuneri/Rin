50872935f042df31f1122ccf346725ab879bfee4
Sakura <69903799+kafuneri@users.noreply.github.com>
2024-10-07 12:02:14 +0800

    Update webhook.ts

--- server/src/utils/webhook.ts
+++ server/src/utils/webhook.ts
@@ -35,7 +35,7 @@ async function sendWebhook(url: string, data: any): Promise<Response> {
  */
 export async function notify(message: string): Promise<void> {
     const webhook_url = 'https://oapi.dingtalk.com/robot/send?access_token=5567b2ed508a9757acca984c5e0826a13e1e8e720d1b351e7eb909ebabb6899f';
-    const secret = 'SEC57abed8a54a5d0ed558c4eef2549f518010edbf9f23d0225b52d16494745fb01';
+    const secret = 'SEC27f4cf22de9cc6dc7962017a73d2e25d5e2668150237459a2e9856b42fd66f53';
 
     if (!webhook_url || !secret) {
         console.error('Please set WEBHOOK_URL and secret');
