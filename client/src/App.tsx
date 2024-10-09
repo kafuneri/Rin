489a00f260fbf5df6aaa566ae7cac1a8e3aa198a
Sakura <69903799+kafuneri@users.noreply.github.com>
2024-10-09 08:41:53 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -68,7 +68,7 @@ function App() {
         { src: "https://npm.elemecdn.com/aplayer@1.10.1/dist/APlayer.min.js" },
         { src: "https://npm.elemecdn.com/meting@2.0.1/dist/Meting.min.js" },
       ];
-      const live2dScript = { src: "https://zhangshuqiao.org/lib/custom.js" };
+      const live2dScript = { src: "https://image.kafuchino.top/api/rfile/custom.js" };
 
       Promise.all(musicScripts.map(script => new Promise<void>((resolve, reject) => {
         const scriptElement = document.createElement('script');
