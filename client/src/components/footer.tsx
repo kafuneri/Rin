675ab1a2d1c6030de2e1c23867f9cec4b232a6a1
Sakura <69903799+kafuneri@users.noreply.github.com>
2024-10-09 20:20:53 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -7,97 +7,107 @@ import { useTranslation } from "react-i18next";
 import { fetchCountAndUpdateUI } from '../utils/count';
 
 type ThemeMode = 'light' | 'dark' | 'system';
+function Footer() {
+    const { t } = useTranslation()
+    const [modeState, setModeState] = useState<ThemeMode>('system');
+    const config = useContext(ClientConfigContext);
+    const footerHtml = config.get<string>('footer');
+    useEffect(() => {
+        const mode = localStorage.getItem('theme') as ThemeMode || 'system';
+        setModeState(mode);
+        setMode(mode);
+        fetchCountAndUpdateUI()
+    }, [])
 
-type FooterProps = {
-  global_pv: number; // 全站访问量
-  global_uv: number; // 全站独立访客数
-};
+    const setMode = (mode: ThemeMode) => {
+        setModeState(mode);
+        localStorage.setItem('theme', mode);
 
-const Footer: React.FC<FooterProps> = ({ global_pv, global_uv }) => {
-  const { t } = useTranslation();
-  const [modeState, setModeState] = useState<ThemeMode>('system');
-  const config = useContext(ClientConfigContext);
-  const footerHtml = config.get<string>('footer');
 
-  useEffect(() => {
-    const mode = localStorage.getItem('theme') as ThemeMode || 'system';
-    setModeState(mode);
-    setMode(mode);
-    fetchCountAndUpdateUI();
-  }, []);
+        if (mode !== 'system' || (!('theme' in localStorage) && window.matchMedia(`(prefers-color-scheme: ${mode})`).matches)) {
+            document.documentElement.setAttribute('data-color-mode', mode);
+        } else {
+            const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+            if (mediaQuery.matches) {
+                document.documentElement.setAttribute('data-color-mode', 'dark');
+            } else {
+                document.documentElement.setAttribute('data-color-mode', 'light');
+            }
+        }
+        window.dispatchEvent(new Event("colorSchemeChange"));
+    };
 
-  const setMode = (mode: ThemeMode) => {
-    setModeState(mode);
-    localStorage.setItem('theme', mode);
-    document.documentElement.setAttribute('data-color-mode', mode);
-    window.dispatchEvent(new Event("colorSchemeChange"));
-  };
+    return (
+        <footer>
+            <Helmet>
+                <link rel="alternate" type="application/rss+xml" title={siteName} href="/sub/rss.xml" />
+                <link rel="alternate" type="application/atom+xml" title={siteName} href="/sub/atom.xml" />
+                <link rel="alternate" type="application/json" title={siteName} href="/sub/rss.json" />
+            </Helmet>
+            <div className="flex flex-col mb-8 space-y-2 justify-center items-center t-primary ani-show">
+                {footerHtml && <div dangerouslySetInnerHTML={{ __html: footerHtml }} />}
+                    <div className="w-fit-content inline-flex rounded-full border border-zinc-200 p-[3px] dark:border-zinc-700">
+                    <ThemeButton mode='light' current={modeState} label="Toggle light mode" icon="ri-sun-line" onClick={setMode} />
+                    <ThemeButton mode='system' current={modeState} label="Toggle system mode" icon="ri-computer-line" onClick={setMode} />
+                    <ThemeButton mode='dark' current={modeState} label="Toggle dark mode" icon="ri-moon-line" onClick={setMode} />
+                   </div>
+                <p className='text-sm text-neutral-500 font-normal text-center'>
+                    {t('count.site_pv')} <span id="site_pv"></span> | {t('count.site_uv')} <span id="site_uv"></span>
+                </p>
+                <p className='text-sm text-neutral-500 font-normal link-line text-center'>
+                    <span>
+                        © 2024 <a className='hover:underline' href="https://www.kafuchino.top" target="_blank">Chino</a>
+                    </span>
+                    {config.get<boolean>('rss') && <>
+                        <Spliter />
+                        <Popup trigger={
+                            <button className="hover:underline" type="button">
+                                RSS
+                            </button>
+                        }
+                            position="top center"
+                            arrow={false}
+                            closeOnDocumentClick>
+                            <div className="border-card">
+                                <p className='font-bold t-primary'>
+                                    {t('footer.rss')}
+                                </p>
+                                <p>
+                                    <a href='/sub/rss.xml'>
+                                        RSS
+                                    </a> <Spliter />
+                                    <a href='/sub/atom.xml'>
+                                        Atom
+                                    </a> <Spliter />
+                                    <a href='/sub/rss.json'>
+                                        JSON
+                                    </a>
+                                </p>
 
-  return (
-    <footer>
-      <Helmet>
-        <link rel="alternate" type="application/rss+xml" title={siteName} href="/sub/rss.xml" />
-        <link rel="alternate" type="application/atom+xml" title={siteName} href="/sub/atom.xml" />
-        <link rel="alternate" type="application/json" title={siteName} href="/sub/rss.json" />
-      </Helmet>
-      <div className="flex flex-col mb-8 space-y-2 justify-center items-center t-primary ani-show">
-        {footerHtml && <div dangerouslySetInnerHTML={{ __html: footerHtml }} />}
-        <div className="w-fit-content inline-flex rounded-full border border-zinc-200 p-[3px] dark:border-zinc-700">
-          <ThemeButton mode='light' current={modeState} label="Toggle light mode" icon="ri-sun-line" onClick={setMode} />
-          <ThemeButton mode='system' current={modeState} label="Toggle system mode" icon="ri-computer-line" onClick={setMode} />
-          <ThemeButton mode='dark' current={modeState} label="Toggle dark mode" icon="ri-moon-line" onClick={setMode} />
-        </div>
-        <p className='text-sm text-neutral-500 font-normal text-center'>
-          {t('count.site_pv')} <span id="site_pv">{global_pv}</span> | {t('count.site_uv')} <span id="site_uv">{global_uv}</span>
-        </p>
-        <p className='text-sm text-neutral-500 font-normal link-line text-center'>
-          <span>
-            © 2024 <a className='hover:underline' href="https://www.kafuchino.top" target="_blank">Chino</a>
-          </span>
-          {config.get<boolean>('rss') && (
-            <>
-              <Spliter />
-              <Popup trigger={
-                <button className="hover:underline" type="button">
-                  RSS
-                </button>
-              }
-                position="top center"
-                arrow={false}
-                closeOnDocumentClick>
-                <div className="border-card">
-                  <p className='font-bold t-primary'>
-                    {t('footer.rss')}
-                  </p>
-                  <p>
-                    <a href='/sub/rss.xml'>RSS</a> <Spliter />
-                    <a href='/sub/atom.xml'>Atom</a> <Spliter />
-                    <a href='/sub/rss.json'>JSON</a>
-                  </p>
-                </div>
-              </Popup>
-            </>
-          )}
-          <br />
-          <a className='hover:underline' href="https://icp.gov.moe/?keyword=20240729" target="_blank"><span className="icon-MOE" />萌ICP备20240729号</a> | <a className='hover:underline' href="https://travel.moe/go.html?travel=on" title="异次元之旅-跃迁-我们一起去萌站成员的星球旅行吧！" target="_blank">异次元之旅</a>
-          <br />Powered by <a className='hover:underline' href="https://github.com/liuran001/Rin" target="_blank">Rin</a> & <a className='hover:underline' href="https://www.cloudflare.com" target="_blank">Cloudflare</a>
-        </p>
-      </div>
-    </footer>
-  );
-};
+                            </div>
+                        </Popup>
+                    </>}
+                     <br/>
+                    <a className='hover:underline' href="https://icp.gov.moe/?keyword=20240729" target="_blank"><span className="icon-MOE"/>萌ICP备20240729号</a> | <a className='hover:underline' href="https://travel.moe/go.html?travel=on" title="异次元之旅-跃迁-我们一起去萌站成员的星球旅行吧！" target="_blank">异次元之旅</a>
+                    <br/>Powered by <a className='hover:underline' href="https://github.com/liuran001/Rin" target="_blank">Rin</a> & <a className='hover:underline' href="https://www.cloudflare.com" target="_blank">Cloudflare</a>
+                    </p>               
+            </div>            
+        </footer>
+    );
+}
 
 function Spliter() {
-  return (<span className='px-1'>|</span>);
+    return (<span className='px-1'>
+        |
+    </span>
+    )
 }
 
 function ThemeButton({ current, mode, label, icon, onClick }: { current: ThemeMode, label: string, mode: ThemeMode, icon: string, onClick: (mode: ThemeMode) => void }) {
-  return (
-    <button aria-label={label} type="button" onClick={() => onClick(mode)}
-      className={`rounded-inherit inline-flex h-[32px] w-[32px] items-center justify-center border-0 t-primary ${current === mode ? "bg-w rounded-full shadow-xl shadow-light" : ""}`}>
-      <i className={`${icon}`} />
-    </button>
-  );
+    return (<button aria-label={label} type="button" onClick={() => onClick(mode)}
+        className={`rounded-inherit inline-flex h-[32px] w-[32px] items-center justify-center border-0 t-primary ${current === mode ? "bg-w rounded-full shadow-xl shadow-light" : ""}`}>
+        <i className={`${icon}`} />
+    </button>)
 }
 
 export default Footer;
