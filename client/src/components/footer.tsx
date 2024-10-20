25dc29607f162c410ee7c8c155ce16847164740b
Chino <69903799+kafuneri@users.noreply.github.com>
2024-10-20 18:23:10 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -88,7 +88,7 @@ function Footer() {
                         </Popup>
                     </>}
                      <br/>
-                    <a className='hover:underline' href="https://icp.gov.moe/?keyword=20240729" target="_blank"><span className="icon-MOE"/>萌ICP备20240729号</a> | <a className='hover:underline' href="https://travel.moe/go.html?travel=on" title="异次元之旅-跃迁-我们一起去萌站成员的星球旅行吧！" target="_blank">异次元之旅</a>
+                    <a className='hover:underline' href="https://icp.gov.moe/?keyword=20243666" target="_blank"><span className="icon-MOE"/>萌ICP备20243666号</a> | <a className='hover:underline' href="https://travel.moe/go.html?travel=on" title="异次元之旅-跃迁-我们一起去萌站成员的星球旅行吧！" target="_blank">异次元之旅</a>
                     <br/>Powered by <a className='hover:underline' href="https://github.com/kafuneri/Rin" target="_blank">Rin</a> & <a className='hover:underline' href="https://www.cloudflare.com" target="_blank">Cloudflare</a>
                     </p>               
             </div>            
