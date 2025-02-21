40541a9e4952bd788fc9757e6bede842e037e959
Chino <69903799+kafuneri@users.noreply.github.com>
2025-02-21 22:13:58 +0800

    添加一言标语

---
+++ client/src/utils/hitokoto.ts
@@ -0,0 +1,24 @@
+let loading = false;
+let cachedHitokoto: string | null = null;
+
+export async function fetchHitokoto() {
+    if (loading) return;
+    loading = true;
+    if (cachedHitokoto !== null) {
+        updateHitokotoElements(cachedHitokoto);
+        loading = false;
+        return;
+    }
+    const response = await fetch('https://v1.hitokoto.cn/?encode=text');
+    const hitokotoText = await response.text();
+    cachedHitokoto = hitokotoText;
+    updateHitokotoElements(hitokotoText);
+    loading = false;
+}
+
+function updateHitokotoElements(hitokotoText: string) {
+    const hitokotos = document.querySelectorAll('[id="hitokoto_text"]');
+    hitokotos.forEach(hitokoto => {
+        hitokoto.innerHTML = hitokotoText;
+    });
+}
